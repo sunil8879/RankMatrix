4c1e57d84444fedc63f46f595e6e3fc32ca90ad3
sunil8879 <sunilvasarkar@yahoo.com>
2026-05-10 01:01:50 +0530

    Delete config.example.js

--- config.example.js
+++
@@ -1,6 +0,0 @@
-// Template file for others to see the structure
-const API_CONFIG = {
-    UNIHUNT_KEY: "PASTE_YOUR_KEY_HERE",
-    ADZUNA_ID: "PASTE_YOUR_ID_HERE",
-    ADZUNA_KEY: "PASTE_YOUR_KEY_HERE"
-};
